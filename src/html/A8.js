b81e59058fe4ec152168be437d172e355381d659
aditya <adityapandeyadp@gmail.com>
2022-02-03 09:25:29 +0530

    a8 mistake fine

--- src/html/A8.js
+++ src/html/A8.js
@@ -1,38 +1,38 @@
 import React from 'react';
 import '../css/A1.css';
 
-const A8 =() => {
+const A8 = () => {
     return (
         <>
-        <div className="row">
-        <div className="column left first">
-            
-<h1>India says it will launch digital rupee as soon as this year</h1>
-<p><b>India's finance minister has said the country will launch a digital version of the rupee as early as this year.</b></p>
-<p>In her annual budget speech, Nirmala Sitharaman also outlined plans for a 30% tax on income from digital assets.</p>
+            <div className="row">
+                <div className="column left first">
 
-<p>It would put profits from trading or transferring cryptocurrencies and non-fungible tokens in the country's highest tax band.</p>
+                    <h1>India says it will launch digital rupee as soon as this year</h1>
+                    <p><b>India's finance minister has said the country will launch a digital version of the rupee as early as this year.</b></p>
+                    <p>In her annual budget speech, Nirmala Sitharaman also outlined plans for a 30% tax on income from digital assets.</p>
 
-<p>India is the latest major economy to announce an official virtual currency, as China trials the digital yuan.</p>
+                    <p>It would put profits from trading or transferring cryptocurrencies and non-fungible tokens in the country's highest tax band.</p>
 
-<p>"Introduction of a central bank digital currency will give a boost, a big boost to (the) digital economy," Ms Sitharaman said on Tuesday.</p>
+                    <p>India is the latest major economy to announce an official virtual currency, as China trials the digital yuan.</p>
 
-<p>"Digital currency will also lead to a more efficient and cheaper currency management system," she added.</p>
+                    <p>"Introduction of a central bank digital currency will give a boost, a big boost to (the) digital economy," Ms Sitharaman said on Tuesday.</p>
 
-<p>Ms Sitharaman also said the magnitude and frequency of digital asset transactions "have made it imperative to provide for a specific tax regime", where profits from transactions are taxed.</p>
+                    <p>"Digital currency will also lead to a more efficient and cheaper currency management system," she added.</p>
 
-<p>The tax would also apply to gifts of digital assets, with recipients being liable to pay the levy. Taxes for all other transactions would be deducted at source.</p>
+                    <p>Ms Sitharaman also said the magnitude and frequency of digital asset transactions "have made it imperative to provide for a specific tax regime", where profits from transactions are taxed.</p>
 
-<p>The announcement came as Ms Sitharaman unveiled the Indian federal government's <b>annual budget.</b></p>
+                    <p>The tax would also apply to gifts of digital assets, with recipients being liable to pay the levy. Taxes for all other transactions would be deducted at source.</p>
 
-<p>It included ramping up spending on infrastructure and extended credit guarantees to struggling small businesses.
+                    <p>The announcement came as Ms Sitharaman unveiled the Indian federal government's <b>annual budget.</b></p>
 
-<p>Asia's third largest economy has been hit hard by the pandemic with major job losses and rising inflation.</p>
+                    <p>It included ramping up spending on infrastructure and extended credit guarantees to struggling small businesses.</p>
+
+                    <p>Asia's third largest economy has been hit hard by the pandemic with major job losses and rising inflation.</p>
 
 
-
-        </div>
-        </>
-    )
+                </div>
+                </div>
+            </>
+            )
 }
-export default A8;
+            export default A8;
