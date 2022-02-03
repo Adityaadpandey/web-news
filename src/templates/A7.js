f717d8b0837e01a2a075054580062e9a84ef0213
aditya <adityapandeyadp@gmail.com>
2022-02-03 09:30:26 +0530

    temp

--- src/templates/A7.js
+++ src/templates/A7.js
@@ -3,14 +3,14 @@ import a7img from '../images/a7im1.jpg';
 
 
 
-const A7= () => {
+const A7 = () => {
     return (
         <>
             <div className="last">
                 <a href="/7">
-                <h1>Epic backed by 35 US states in Apple legal rematch
-     </h1>
-     <img src={a7img} alt="" />
+                    <h1>Epic backed by 35 US states in Apple legal rematch
+                    </h1>
+                    <img src={a7img} alt="" />
                 </a>
             </div>
 
