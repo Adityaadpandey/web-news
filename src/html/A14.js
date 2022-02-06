50fae6af58dc60047015ffde3458b6b2b35d2788
adityapandey <adityapandeyadp@gmail.com>
2022-02-06 17:08:45 +0530

    a14 in prog

---
+++ src/html/A14.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const A14 = () => {
+    return (
+        <>
+            <div className="row">
+                <div className="column left first ">
+                    
+                </div>
+            </div>
+        </>
+  );
+};
+
+export default A14;
