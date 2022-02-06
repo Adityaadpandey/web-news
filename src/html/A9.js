6e2fc8eb1439c8742b6bbf1a313b050ff6c0a712
adityapandey <adityapandeyadp@gmail.com>
2022-02-06 15:58:19 +0530

    a9 is done

--- src/html/A9.js
+++ src/html/A9.js
@@ -1,8 +1,13 @@
 import React from "react";
-import a9im1 from "../images/a9im.jpeg"
-import a9im2 from "../images/a9im2.jpeg"
+import a9im1 from "../images/a9im.jpeg";
+import a9im2 from "../images/a9im2.jpeg";
+import a9im3 from "../images/a9im3.jpeg"
+import a9im4 from "../images/a9im4.jpg"
+import A1temp from "../templates/A2";
+import A3temp from "../templates/A3";
+import A4temp from "../templates/A5";
+import A5temp from "../templates/A3";
 
-;
 
 const A9 = () => {
   return (
@@ -10,7 +15,7 @@ const A9 = () => {
       <div className="row">
         <div className="column left first">
           <h1>Meta moves to tackle creepy behaviour in virtual reality</h1>
-<img src={a9im1} alt="" />
+          <img src={a9im1} alt="" />
           <p>
             <b>
               Meta has announced a new feature to allow more personal space for
@@ -44,47 +49,163 @@ const A9 = () => {
           <p>
             It stops others "invading your avatar's personal space", said Meta.
           </p>
-<p>"If someone tries to enter your Personal Boundary, the system will halt their forward movement as they reach the boundary." </p>
+          <p>
+            "If someone tries to enter your Personal Boundary, the system will
+            halt their forward movement as they reach the boundary."{" "}
+          </p>
 
-<p>It is being made available in Meta's Horizon Worlds and Horizon Venues software. </p>
+          <p>
+            It is being made available in Meta's Horizon Worlds and Horizon
+            Venues software.{" "}
+          </p>
 
-<p>The firm said it was a "powerful example of how VR has the potential to help people interact comfortably," but acknowledged there was more work to be done.</p>
-<h2>Staring
-</h2>
-<p>"I did have some moments when it was awkward for me as a woman," Ms Olson said of her interactions in virtual reality (VR).</p>
+          <p>
+            The firm said it was a "powerful example of how VR has the potential
+            to help people interact comfortably," but acknowledged there was
+            more work to be done.
+          </p>
+          <h2>Staring</h2>
+          <p>
+            "I did have some moments when it was awkward for me as a woman," Ms
+            Olson said of her interactions in virtual reality (VR).
+          </p>
 
-<p>She was visiting Meta's Horizon Worlds, its virtual-reality platform where anyone 18 or older can create an avatar and hang out.</p>
+          <p>
+            She was visiting Meta's Horizon Worlds, its virtual-reality platform
+            where anyone 18 or older can create an avatar and hang out.
+          </p>
 
-<p>To do so, users need one of Meta's VR headsets, and the space offers the chance to play games and chat to other avatars, none of whom has legs.</p>
+          <p>
+            To do so, users need one of Meta's VR headsets, and the space offers
+            the chance to play games and chat to other avatars, none of whom has
+            legs.
+          </p>
 
-<p>"I could see straight away I was the only woman, the only female avatar. And I had these men kind of come around me and stare at me silently," Ms Olson told Tech Tent.</p>
+          <p>
+            "I could see straight away I was the only woman, the only female
+            avatar. And I had these men kind of come around me and stare at me
+            silently," Ms Olson told Tech Tent.
+          </p>
 
-<p>"Then they started taking pictures of me and giving the pictures to me and I had a moment when a guy zoomed up to me and said something.</p>
+          <p>
+            "Then they started taking pictures of me and giving the pictures to
+            me and I had a moment when a guy zoomed up to me and said something.
+          </p>
 
-<p>"And in virtual reality, if someone is close to you, then the voice sounds like someone is literally talking into your ear. And it took me aback."</p>
+          <p>
+            "And in virtual reality, if someone is close to you, then the voice
+            sounds like someone is literally talking into your ear. And it took
+            me aback."
+          </p>
 
-<p>She experienced similar discomfort in Microsoft's social VR platform.</p>
+          <p>
+            She experienced similar discomfort in Microsoft's social VR
+            platform.
+          </p>
 
-<p>"I was talking to another lady and within minutes of us chatting a guy came up and started chatting to us and following us around saying inappropriate things and we had to block him," she said.</p>
+          <p>
+            "I was talking to another lady and within minutes of us chatting a
+            guy came up and started chatting to us and following us around
+            saying inappropriate things and we had to block him," she said.
+          </p>
 
-<p>"I have since heard of other women who have had similar experiences."</p>
+          <p>
+            "I have since heard of other women who have had similar
+            experiences."
+          </p>
 
-<p>She said while she wouldn't describe it as harassment, it was "creepy and awkward".</p>
+          <p>
+            She said while she wouldn't describe it as harassment, it was
+            "creepy and awkward".
+          </p>
 
-<p>Nina Jane Patel went a lot further this week when she told the Daily Mail that she was abused in Horizon Venues, likening it to sexual assault. She described how a group of male avatars "groped her" and subjected her to a stream of sexual innuendo. They photographed her and sent a message reading: "Don't pretend you didn't love it."</p>
+          <p>
+            Nina Jane Patel went a lot further this week when she told the Daily
+            Mail that she was abused in Horizon Venues, likening it to sexual
+            assault. She described how a group of male avatars "groped her" and
+            subjected her to a stream of sexual innuendo. They photographed her
+            and sent a message reading: "Don't pretend you didn't love it."
+          </p>
 
-<p>Meta responded to the paper saying that it was sorry. "We want everyone to have a positive experience and easily find the safety tools that can help in a situation like this - and help us investigate and take action."</p>
-<img src={a9im2} alt="" />
-<p>Moderating content in the nascent metaverse is going to be challenging, and Meta's chief technology officer, Andrew Bosworth, admitted that it would offer both "greater opportunities and greater threats".</p>
+          <p>
+            Meta responded to the paper saying that it was sorry. "We want
+            everyone to have a positive experience and easily find the safety
+            tools that can help in a situation like this - and help us
+            investigate and take action."
+          </p>
+          <img src={a9im2} alt="" />
+          <p>
+            Moderating content in the nascent metaverse is going to be
+            challenging, and Meta's chief technology officer, Andrew Bosworth,
+            admitted that it would offer both "greater opportunities and greater
+            threats".
+          </p>
 
-<p>"It could feel a lot more real to me, if you were being abusive towards me because it feels a lot more like physical space," he said in an interview with the BBC late last year.</p>
+          <p>
+            "It could feel a lot more real to me, if you were being abusive
+            towards me because it feels a lot more like physical space," he said
+            in an interview with the BBC late last year.
+          </p>
 
-<p>But he said people in virtual roles would have "a great deal more power" over their environments.</p>
+          <p>
+            But he said people in virtual roles would have "a great deal more
+            power" over their environments.
+          </p>
 
-<p>"If I were to mute you, you would cease to exist for me and your ability to do harm to me is immediately nullified."</p>
+          <p>
+            "If I were to mute you, you would cease to exist for me and your
+            ability to do harm to me is immediately nullified."
+          </p>
+<p>And he questioned whether people would want the kind of moderation that exists on platforms such as Facebook when having chats in virtual reality.</p>
 
 
-        </div>
+<p>"Do you really want the system or a person standing by listening in? Probably not"</p>
+
+
+<p>"So I think we have a privacy trade-off - if you want to have a high degree of content, safety or what we would call integrity, well that trades off against privacy."</p>
+
+
+<p>And in Meta's vision of the metaverse, where different rooms are run by different companies, the trade-off gets even more complex as people move out of the Meta-controlled virtual world into others.</p>
+
+
+<p>"I can give no guarantees about either the privacy, nor the integrity of that conversation," he said.</p>
+<img src={a9im3}alt="" />
+<p>Ms Olson agreed that it was going to be "a very difficult thing for Facebook, Microsoft and others to take care of".</p>
+
+
+<p>"When you are scanning text for hate speech, it's hard but doable - you can use machine-learning algorithms.</p>
+
+
+<p>"To process visual information about an avatar or how close one is to another, that is going to be so expensive computationally, that is going to take up so much computer power, I don't know what technology can do that."</p>
+
+
+<p>Facebook is investing $10bn in its metaverse plans and part of that will need to go on building new ways of moderating content.</p>
+
+
+<p>"We have learned a tremendous amount in the last 15 years of online discourse... so we're going to bring all that knowledge with us to do the best that we can to build these things from the ground up, to give people a lot of control over their own experience," Mr Bosworth told the BBC.</p>
+<img src={a9im4} alt="" />
+<p>Dr Beth Singler, an anthropologist at Cambridge University, who has studied the ethics of virtual worlds, said: "Facebook has already failed to learn about what is happening in online spaces. Yes, they have changed some of their policies but there is still material out there that shouldn't be."</p>
+
+<p>There is more to learn from gaming, she thinks, where the likes of Second Life and World of Warcraft have offered virtual worlds for years, limiting who avatars can talk to and the names they can choose for them.</p>
+
+<p>Meta's decision to use legless avatars may also be deliberate, she thinks - most likely a technical one about the lack of sensors for legs, but it could also be a way to limit "below the belt" issues that might arise from having a fully physical presence.</p>
+
+<p>But having strict rules around what avatars can look like may bring its own problems for those "trying to express a certain identity," she added.</p>
+
+
+          </div>
+          <div class="column right first" >
+  <A1temp />
+  </div>
+  <div class="column right first" >
+  <A3temp />
+  </div>
+  <div class="column right first" >
+  <A4temp />
+  </div>
+  <div class="column right first" >
+  <A5temp />
+  </div>
       </div>
     </>
   );
