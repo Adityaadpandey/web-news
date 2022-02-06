0f9f2163493af0e0bcbefeb4b257129a3aa940d9
adityapandey <adityapandeyadp@gmail.com>
2022-02-06 22:03:31 +0530

    a15 is done

---
+++ src/html/A15.js
@@ -0,0 +1,161 @@
+import React from "react";
+import a15im1 from "../images/a15im1.jpg";
+import a15im2 from "../images/a15im2.jpg";
+import a15im3 from "../images/a15im3.jpg";
+import a15im4 from "../images/a15im4.jpg";
+
+const A15 = () => {
+  return (
+    <>
+      <div className="row">
+        <div className="column first left">
+          <h1>How to store excess wind power underwater</h1>
+          <img src={a15im1} alt="" />
+          <p>
+            <b>
+              With 17 new wind farm projects planned for Scotland, the UK's
+              offshore wind power capacity is set to more than double
+            </b>
+          </p>
+          <p>
+            But what happens when the wind is blowing, the turbines are cranking
+            out electricity and there's no demand for it?
+          </p>
+
+          <p>
+            It's already a problem. In 2020, enough electricity to supply more
+            than one million homes was wasted due to a lack of storage,
+            <b>according to a report by KPMG</b>that was commissioned by the
+            power company Drax.
+          </p>
+
+          <p>
+            But one firm, which won a 2022 Best of Innovation award at the CES
+            technology show earlier this year, believes it has the solution.
+          </p>
+
+          <p>
+            Dutch startup, Ocean Grazer, has developed the Ocean Battery, which
+            stores energy below the wind farm.
+          </p>
+
+          <p>
+            When there is excess electricity the system pumps water from an
+            underground reservoir into tough, flexible bladders that sit on the
+            sea bed. You could think of them like big bicycle inner tubes.
+          </p>
+          <img src={a15im2} alt="" />
+          <p>
+            The water in those tubes is under pressure, so when it is released
+            the water flows quickly and is directed through turbines, also on
+            the sea bed, generating electricity when needed.
+          </p>
+
+          <p>
+            "The Ocean Battery, is effectively based on the same technology as
+            hydro storage, where water is pumped back through a dam in a river,
+            though we have transformed it into something you can deploy on the
+            sea bed," says chief executive Frits Bliek.
+          </p>
+
+          <p>
+            A protoype designed for deep water has already been tested at the
+            port of Groningen in the Netherlands. The firm is now preparing to
+            test a second system that has been modified for shallower water in a
+            lake in the northern Netherlands. This should be operational next
+            year.
+          </p>
+
+          <p>
+            Many on-shore wind farms already use batteries to store extra power
+            but there are a number of problems when it comes to using these
+            offshore.
+          </p>
+
+          <p>
+            "If you want to build a large-scale system offshore, you would also
+            need to build a really huge platform built of sea containers and
+            that's been found to be very, very costly. Also the lifetime of the
+            batteries is not very long," says Mr Bliek.
+          </p>
+          <img src={a15im3} alt="" />
+          <p>
+            While lithium-ion batteries can last for 5,000-10,000 charging
+            cycles, the Ocean Battery can take up to a million, he says. Though
+            the cost of storage is roughly the same, this extended life makes it
+            much cheaper overall.
+          </p>
+
+          <p>
+            "We found it can be built in shallow waters everywhere where wind
+            farms are being built, and that's also pretty cost-effective at the
+            end of the day," says Mr Bliek.
+          </p>
+
+          <p>
+            He also thinks the technology can be used alongside solar farms, if
+            there is a lake nearby.
+          </p>
+          <p>
+            Dr Gavin Harper, Faraday Institution research fellow at Birmingham
+            University, says the future of energy storage is likely to involve a
+            mix of different techniques.
+          </p>
+
+          <p>
+            "Different energy storage technologies are suitable for storing
+            energy on different time scales. Some are better suited for very
+            fast responsive energy storage and others are suited to longer-term
+            energy storage."
+          </p>
+
+          <p>
+            "One of the real advantages with this type of technology is it
+            eliminates the use of critical materials - for example, cobalt,
+            nickel and lithium - which are key technology metals used in the
+            manufacture of lithium-ion batteries."
+          </p>
+
+          <p>Such materials are expensive and in short supply.</p>
+          <img src={a15im4} alt="" />
+          <p>
+            But there are concerns about the financial viability of the system,
+            says reader in energy systems and policy, Dr Jonathan Radcliffe, who
+            is also at Birmingham University.
+          </p>
+
+          <p>
+            "In principle, the technology is aiming to store large amounts of
+            energy over periods of many hours, days or weeks. The problem that a
+            lot of energy storage technologies face is that the value of storing
+            energy at that scale is not high in the current market, so it would
+            take a long time to pay back."
+          </p>
+
+          <p>
+            Land-based battery storage is more useful to power firms as it can
+            be used to handle other problems with the grid, such as when power
+            lines are near their limits, he says.
+          </p>
+
+          <p>
+            "Those applications are quite highly valued and need storage over
+            short periods of time, so quite a few projects are proving to be
+            commercial."
+          </p>
+
+          <p>
+            Last year, the UK government launched a competition to come up with
+            innovative energy storage solutions, with up to £68m ($93m)
+            available in capital funding. Projects are expected to include
+            electric, thermal and power-to-x, which involves using electricity
+            to create hydrogen or synthetic fuels.
+                  </p>
+                  
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default A15;
