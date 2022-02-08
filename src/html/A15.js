53ac6fb4b79b807702a4b0223a02cbe6c40f234e
adityapandey <adityapandeyadp@gmail.com>
2022-02-08 21:44:54 +0530

    15 has some prb

--- src/html/A15.js
+++ src/html/A15.js
@@ -1,12 +1,66 @@
 import React from "react";
+import a15im1 from "../images/a15im1.jpg";
 
 const A15 = () => {
   return (
     <>
       <div className="row">
         <div className="column left first">
-          <h1></h1>
+
+          <h1>Meta told to overhaul policies over doxxing fears</h1>
+
+          <img src={a15im1} alt="" />
+          
+          <p>
+            <b>
+              Meta's Oversight Board has advised the social network to change
+              its policy on allowing the sharing of people's addresses, even if
+              the information is considered public.
+            </b>
+          </p>
+          <p>
+            Meta requested the advice last year - the first time it has asked
+            the board to help define one of its policies.
+          </p>
+
+          <p>
+            It follows a series of leaks on <p>social media of high-profile
+            people's addresses.
+          </p>
+
+          <p>
+            This so-called doxxing could cause real-world harms, the board
+            advised.
+          </p>
+
+          <p>
+            Doxxing refers to the release of private information about
+            individuals online, usually with malicious intent.
+          </p>
+            <h2>Tricky reality</h2>
+            
+          <p>In November, Harry Potter author JK Rowling said she was a victim - after a photograph taken outside her home, revealing its address, was circulated online.</p>
+          <p>But in January, police said no action would be taken against the activists who had targeted her.</p>
+
+          <p>Facebook, owned by Meta, does not allow the sharing of private residential information generally - unless it is already "publicly available".</p>
+
+          <p>But in June, it asked the board to further consider the tricky reality that while potentially relevant to journalism and activism, access to such information could also risk residents' safety.
+
+            </p>
+            <h2>Malicious sharing</h2>
+
+<p>In response, the Oversight Board has issued a detailed 17-point plan and said the "publicly available" exception should no longer apply.</p>
+
+<p>It also said Meta should "swiftly" respond to anyone claiming to have fallen victim to doxxing, regardless of whether they had Facebook account.</p>
+
+<p>"The malicious sharing of private residential addresses on social media is a serious problem that can lead to real-world harms, including stalking and harassment," said Oversight Board director Thomas Hughes.</p>
+
+<p>Meta will not have to implement the recommendations - but it must respond within 60 days.
+
+            </p>
+            
         </div>
+        
       </div>
     </>
   );
