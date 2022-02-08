0d3abb8bd3cfed459a47ab1770111f7c7fe52532
adityapandey <adityapandeyadp@gmail.com>
2022-02-08 21:55:30 +0530

    a17 is done

--- src/html/A17.js
+++ src/html/A17.js
@@ -1,11 +1,115 @@
 import React from "react";
+import a17im1 from "../images/a17im1.jpg";
 
 const A17 = () => {
   return (
     <>
       <div className="row">
         <div className="column left first ">
-          <h1></h1>
+          <h1>
+            UK chip firm Arm sale by Softbank collapses amid competition fears
+          </h1>
+          <img src={a17im1} alt="" />
+          <p>
+            <b>
+              Japanese conglomerate SoftBank has called off its planned sale of
+              UK microchip designer Arm to US technology group Nvidia.
+            </b>
+          </p>
+          <p>
+            When the deal was first announced in September 2020 it was valued at
+            around $40bn (£29.6bn).
+          </p>
+
+          <p>
+            SoftBank now aims to float Arm's shares on the stock market by the
+            end of March next year.
+          </p>
+
+          <p>
+            The planned sale had faced major regulatory hurdles in the UK,
+            United States and European Union.
+          </p>
+
+          <p>
+            SoftBank and Nvidia agreed to end their sale agreement "because of
+            significant regulatory challenges preventing the consummation of the
+            transaction, despite good faith efforts by the parties," the
+            companies said in a joint statement to investors.
+          </p>
+
+          <p>
+            "We will take this opportunity and start preparing to take Arm
+            public, and to make even further progress," SoftBank's chief
+            executive Masayoshi Son added.
+          </p>
+
+          <p>
+            SoftBank did not give any other details about Arm's planned stock
+            market listing, which has sparked speculation amongst investors over
+            which stock exchange, or exchanges, will be chosen for the sale.
+          </p>
+
+          <p>
+            The statement also said that, in line with the agreement signed by
+            both firms in 2020, SoftBank would keep a $1.25bn non-refundable
+            deposit paid by Nvidia.
+          </p>
+          <h2>Under scrutiny</h2>
+          <p>
+            In December, the US Federal Trade Commission sued to block the
+            buyout. It argued that competition in the growing market for
+            microprocessors for self-driving cars and a new category of
+            networking chips could be hurt if Nvidia owned Arm.
+          </p>
+
+          <p>
+            The planned takeover was also being scrutinised by UK and EU
+            regulators amid concerns that it could push up chip prices and
+            reduce choice and innovation.
+          </p>
+
+          <p>
+            Meanwhile, Arm's chief executive Simon Segars, who has been in the
+            role since 2013, is stepping down.
+          </p>
+
+          <p>
+            He will be replaced by Rene Haas, who most recently served as
+            president of intellectual property at Arm.
+          </p>
+
+          <p>
+            "With the uncertainty of the past several months behind us, we are
+            emboldened by a renewed energy to move into a growth strategy and
+            change lives around the world," Mr Haas said.
+          </p>
+
+          <p>
+            Mr Segars, who has been at the company for three decades, will stay
+            on in an advisory role to help with the transition.
+          </p>
+          <p>Arm's chip designs are at the heart of most smartphones and smart devices around the world.
+
+          <p>It licenses its technology to major companies including Apple, Samsung and Qualcomm.</p>
+
+          <p>The company was incorporated in 1990 and made its London Stock Exchange debut eight years later.</p>
+
+          <p>In 2016, it was bought by SoftBank, which is one of the world's biggest technology companies.</p>
+
+          <p>In 2020, SoftBank decided to sell Arm to help shore up its finances after losing money on other investments, including shared office space firm WeWork and ride-hailing platform Uber.</p>
+
+          <p>In recent years, Nvidia has become America's most valuable chip company on the strength of its graphic processors.</p>
+
+          <p>Graphic processors are crucial for video gaming, as well as becoming more widely used for other areas of advanced computing including artificial intelligence applications.</p>
+
+          <p>Nvidia's attempt to take over Arm came against the backdrop of the global chips shortage.</p>
+
+          <p>Millions of products from cars to smartphones rely on computer chips but there are not enough currently being produced to meet demand.</p>
+
+          <p>As sales of devices soared during the pandemic, manufacturers have seen stocks of semiconductors plunge.
+
+</p>
         </div>
       </div>
     </>
