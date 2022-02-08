ae0bc3454f49da8eaf89a715d6749fe9aa56d0fa
aditya <adityapandeyadp@gmail.com>
2022-02-08 23:35:09 +0530

    all temp for 17-25

---
+++ src/templates/A18.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import a18im1 from "../images/a18im1.jpeg";
+
+const A18 = () => {
+    return (
+        <>
+            <a href='/18'>
+                <div className="last">
+                <h1>Microsoft's mega-deal worries small video game makers</h1>
+          <img src={a18im1} alt="" />
+
+                </div>
+            </a>
+        </>
+    );
+};
+
+export default A18;
---
+++ src/templates/A19.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import a19im1 from "../images/a19im1.jpg"
+
+const A19 = () => {
+    return (
+        <>
+            <a href='/17'>
+                <div className="last">
+                <h1>
+            Arm-Nvidia deal: UK orders further inquiry
+          </h1>
+          <img src={a19im1} alt="" />
+
+                </div>
+            </a>
+        </>
+    );
+};
+
+export default A19;
