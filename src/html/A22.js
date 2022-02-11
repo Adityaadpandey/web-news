b7cd333edff4a3fa7e635b4af33fc9470959a280
aditya <adityapandeyadp@gmail.com>
2022-02-11 09:50:16 +0530

    okkk

--- src/html/A22.js
+++ src/html/A22.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import a22im1 from "../images/a22im1.webp"
+// import a22im1 from "../images/a22im1.webp"
 
 const A22 = () => {
   return (
@@ -9,7 +9,7 @@ const A22 = () => {
           <h1>
             Safer Internet Day - Acronis shares recommendations to stay safe online
           </h1>
-          <img src={a22im1} alt="" />
+          {/* <img src={a22im1} alt="" /> */}
           <p><b>Modern cyberattacks, data leaks, and ransomware outbreaks have all revealed the same thing: cybersecurity is failing. This failure is the result of weak technologies and human mistakes caused by clever social engineering.</b></p>
           <p>Modern cyberattacks, data leaks, and ransomware outbreaks have all revealed the same thing: cybersecurity is failing. This failure is the result of weak technologies and human mistakes caused by clever social engineering. In cases where a backup solution was working well and wasn't compromised, it usually would take hours and days to restore systems (with data) to an operational state. Backup is essential for when cybersecurity solutions fail; but at the same time, backup solutions can be compromised, disabled, and perform slowly, causing businesses to lose a lot of money due to downtime. To solve these problems, we recommend integrated cyber protection solutions that combine antimalware, EDR, DLP, email security, vulnerability assessment, patch management, RMM, and backup capabilities into a single agent running under a family of Windows operating systems. This integration lets you maintain optimal performance, eliminate compatibility issues, and ensure rapid recovery. If a threat is missed or detected while your data is being altered, the data will be restored from a backup immediately – because of its one agent, it knows that data was lost and needed to be restored. This isn't possible with an antimalware agent separate from a backup product with its own agent. Your antimalware solution may stop the threat, but some data may already be lost. A backup agent won't know about it automatically and, in the best-case scenario, data will be restored slowly — if at all. Kevin Reed, CISO, Acronis shares his recommendations with The Hans India on this Safer Internet Day. In his 20+ years in cyber security, Kevin has been protecting the network of various organisations, including the 3rd largest bank in Europe, where he implemented cryptographic protection. As a CISO and Vice CIO of Yandex, the $10 billion NASDAQ traded search engine, he supervised the company's security strategy. Now CISO of Acronis, Kevin is in charge of developing cutting-edge security solutions and leads the company's Cyber Protection Operation Centres (CPOC) worldwide.</p>
         </div>
--- src/templates/A22.js
+++ src/templates/A22.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import a22im1 from "../images/a22im1.webp"
+// import a22im1 from "../images/a22im1.webp"
 
 const A22 = () => {
     return (
@@ -9,7 +9,7 @@ const A22 = () => {
                 <h1>
             Safer Internet Day - Acronis shares recommendations to stay safe online
           </h1>
-          <img src={a22im1} alt="" />
+          {/* <img src={a22im1} alt="" /> */}
 
                 </div>
             </a>
