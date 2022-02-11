78db88851dd51761859d5725c7c9cddf4017e20f
adityapandey <adityapandeyadp@gmail.com>
2022-02-11 22:24:44 +0530

    a24 done

--- src/html/A24.js
+++ src/html/A24.js
@@ -1,7 +1,274 @@
-import React from 'react';
+import React from "react";
+import a24im1 from "../images/a24im1.jpg";
+import a24im2 from "../images/a24im2.jpg";
+import a24im3 from "../images/a24im3.jpg";
 
 const A24 = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="row">
+        <div className="column left first">
+          <h1>‘Hackers helped me find my lost Bitcoin fortune’</h1>
+          <img src={a24im1} alt="" />
+          <p>
+            <b>Rhonda Kampert was an early adopter.</b>
+          </p>
+          <p>
+            She bought six Bitcoins in 2013, when they cost about $80 (£60)
+            each, and were the chatter of niche corners of the internet.
+          </p>
+
+          <p>
+            "I used to listen to a radio talk show and they started talking
+            about crypto and Bitcoin so I got interested," she says.
+          </p>
+
+          <p>
+            "Back then buying it was so complicated but I fumbled my way through
+            the process and bought my coins."
+          </p>
+
+          <p>
+            Rhonda, who lives in the US state of Illinois, spent some of her
+            digital money over the next year or so, then forgot about it.
+          </p>
+
+          <p>
+            But when she saw headlines late in 2017 announcing that the value of
+            Bitcoin had risen to nearly $20,000 she excitedly went to her
+            computer to log in and cash out.
+          </p>
+
+          <h2>'It was awful'</h2>
+
+          <p>
+            Except there was a problem. She was missing some of the login
+            details for her Bitcoin wallet - a computer program or device that
+            stores a set of secret numbers, or private keys.
+          </p>
+
+          <p>
+            "I realised then that my printout had missed some digits on the end
+            of my wallet identifier. I had a piece of paper with my password but
+            no idea what my wallet ID was," Rhonda says.
+          </p>
+
+          <p>
+            "It was awful. I tried everything for months but it was hopeless. So
+            I kind of gave up."
+          </p>
+
+          <p>
+            Fast forward to last spring and the value of Bitcoin soared above
+            $50,000 - more than 600 times what Rhonda had paid eight years
+            earlier.
+          </p>
+
+          <p>
+            Filled with a renewed determination to find her coins, she hit the
+            internet and came across father and son crypto treasure hunters
+            Chris and Charlie Brooks.
+          </p>
+          <img src={a24im2} alt="" />
+          <p>
+            "After talking to the guys online for a while I trusted them enough
+            to hand over all the details I could remember. Then I waited," she
+            says.
+          </p>
+
+          <p>
+            "Eventually we sat down together on a video call and watched
+            everything happen. Chris opened the wallet and there it was. I just
+            felt so relieved!"
+          </p>
+
+          <p>
+            Rhonda's wallet of three-and-a-half Bitcoin was at that point worth
+            $175,000.
+          </p>
+
+          <p>
+            "I gave Chris and Charlie their 20%, then the first thing I did was
+            take out $10,000 worth of my coins to help my daughter Megan through
+            college."
+          </p>
+
+          <p>
+            She says she's keeping the rest locked away in a hardware wallet - a
+            security device like a USB stick that stores her details offline.
+            The login pin for her new hardware wallet is etched firmly in her
+            memory.
+          </p>
+
+          <p>
+            Rhonda is hoping the coins, currently worth $43,000 each, will go up
+            in value again. She describes them as her retirement fund for when
+            she wants to quit her job as a day trader in stocks and
+            crypto-currencies.
+          </p>
+          <h2>Lost billions</h2>
+          <p>There are plenty of Rhondas out there who need help.</p>
+
+          <p>
+            One estimate from crypto researchers Chainalysis suggests that out
+            of the 18.9 million Bitcoins in circulation, as many as 3.7 million
+            have been lost by owners.
+          </p>
+
+          <p>
+            And in the decentralised world of crypto-currency no-one is in
+            charge - so if you forget your wallet login details there aren't
+            many places you can turn to.
+          </p>
+
+          <p>
+            Chris and Charlie estimate that services like theirs, which use
+            computers to try out hundreds of thousands of possible login ID and
+            password combinations, could recover 2.5% of those lost Bitcoin -
+            worth about $3.9bn.
+          </p>
+
+          <p>
+            Chris started his business, Crypto Asset Recovery, in 2017, but
+            stopped and focused on other projects for a while. He then revived
+            it after a conversation with his son, Charlie, a little over a year
+            ago.
+          </p>
+
+          <p>
+            "I was on a break from college, and I had done some travelling but
+            was just home workshopping business ideas with my dad," says
+            Charlie, now 20.
+          </p>
+
+          <p>
+            "We came up with the idea of restarting the business so over the
+            next few weeks, we brought servers in-house and fired everything
+            back up again."
+          </p>
+
+          <p>
+            Working from a workshop in their seaside family home in New
+            Hampshire, the father and son began receiving more than 100 emails
+            and calls a day from potential clients the last time the price of
+            Bitcoin peaked, in November 2021.
+          </p>
+
+          <p>
+            Now that it has dropped a little they are working through the
+            backlog of cases.
+          </p>
+
+          <p>
+            The business is going so well that Charlie has no intention of
+            finishing his computer science degree. However, the pair joke that
+            they spend their time in perpetual disappointment.
+          </p>
+
+          <p>
+            Starting with the faint memory or jumbled paperwork of their
+            clients, which provide clues about possible login details, they say
+            they are only successful about 30% of the time.
+          </p>
+          <p>Even then, they're often disappointed by what they find.</p>
+
+          <p>
+            "Most of the time we can't actually tell what's inside the wallet so
+            we have to trust the client that there is an amount that's worth the
+            work we put in," Charlie says.
+          </p>
+
+          <p>
+            "We had a case over the summer where the person told us they had 12
+            Bitcoin. We were obviously very professional with him but behind the
+            computer screen we're like jumping around high-fiving each other,
+            all excited about the potential payday.
+          </p>
+
+          <p>
+            "We spent probably 60 hours of computer server time plus about 10
+            hours with the client piecing together all the clues he could give
+            us.
+          </p>
+
+          <p>
+            "Then on the video call we cracked it open - and it was completely
+            empty."
+          </p>
+
+          <p>
+            Chris and Charlie say that only one client has ever underestimated
+            what was inside his wallet. It turned out to be their biggest haul
+            yet - $280,000 of Bitcoin.
+          </p>
+
+          <p>
+            In the past year they say they've recovered Bitcoins totalling
+            "seven figures" in value.
+          </p>
+          <h2>Hardware hacking</h2>
+          <p>
+            The father-and-son team are part of a growing industry of ethical
+            hackers using their skills to help people retrieve mislaid
+            crypto-currency.
+          </p>
+
+          <p>
+            Another is Joe Grand, who started hacking as a teenager and is well
+            known in the hacking community for testifying before the US Senate
+            in 1998 about early internet vulnerabilities under his internet
+            handle, Kingpin.
+          </p>
+          <img src={a24im3} alt="" />
+          <p>
+            He recently made a viral YouTube video showing how he cracked open a
+            hardware wallet containing $2m of a crypto-currency called Theta.
+          </p>
+          <p>
+            It took months of preparation and practice hacks on other hardware
+            wallets for Joe to come up with a method in his workshop in
+            Portland, Oregon.
+          </p>
+
+          <p>
+            He finally succeeded by combining two previously discovered
+            vulnerabilities with the hardware wallet in question, which showed
+            they can "glitch" and give away their pin code if attacked with
+            carefully timed electrical shocks.
+          </p>
+
+          <p>
+            "When you break a crypto-wallet, I mean when you break any security
+            really, it feels like magic no matter how many times you do it," he
+            says.
+          </p>
+
+          <p>
+            "Even though I had proven multiple times in my tests that I could
+            hack this wallet I was worried on the day because you never know
+            what's going to happen and we only had one shot or the whole thing
+            could break and the coins would be lost forever.
+          </p>
+
+          <p>
+            "Cracking the wallet open was a huge adrenaline rush. I guess it's
+            like a technical version of treasure hunting."
+          </p>
+
+          <p>
+            Joe's day job is teaching manufacturers how to secure their products
+            from hackers but he's now set up a wallet-unlocking side-business
+            with his first client, Dan Reich.
+          </p>
+
+          <p>
+            "If there's big enough wallets and big enough treasure to dedicate
+            my time to," he says, "then we're going to try."
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default A24;
