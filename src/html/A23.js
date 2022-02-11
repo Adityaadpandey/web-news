5ddc0c4e9790bc28944138dbceca08825d6899a9
adityapandey <adityapandeyadp@gmail.com>
2022-02-11 22:05:23 +0530

    a23 done

--- src/html/A23.js
+++ src/html/A23.js
@@ -1,7 +1,210 @@
-import React from 'react';
+import React from "react";
+import a23im1 from "../images/a23im1.jpg";
+import a23im2 from "../images/a23im2.png";
+import a23im3 from "../images/a23im3.jpg";
+import a23im4 from "../images/a23im4.jpg";
+import a23im5 from "../images/a23im5.jpg";
 
 const A23 = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="row">
+        <div className="column left first">
+          <h1>Fusion race kicked into high gear by smart tech</h1>
+          <img src={a23im1} alt="" />
+          <p>
+            <b>
+              A US company is speeding up the path to practical fusion energy by
+              using Google's vast computing power.
+            </b>
+          </p>
+          <p>
+            By applying software that can improve on its own, TAE Technologies
+            has cut down tasks that once took two months to just a few hours.
+          </p>
+
+          <p>
+            Google has lent the firm its expertise in "machine learning" in
+            order to help accelerate the timeline for fusion.
+          </p>
+
+          <p>
+            Nuclear fusion promises a plentiful supply of low-carbon energy,
+            using the same process that powers the Sun.
+          </p>
+
+          <p>
+            Existing nuclear power is based on fission, where a heavy chemical
+            element is split to provide a lighter one. Nuclear fusion works by
+            combining two light elements to make a heavier one.
+          </p>
+
+          <p>
+            For fusion to become economically-viable, it must first generate
+            more energy than the amount being put in. But no one has yet reached
+            this point, despite an eight-decade effort to "build a star on
+            Earth". The challenges are immense, but some in the fusion community
+            hope that new thinking and disruptive technologies could help
+            shatter this paradigm.
+          </p>
+          <p>
+            "I want to deliver fusion first, but anyone who does it is a hero,"
+            TAE's chief executive Dr Michl Binderbauer told BBC News.
+          </p>
+          <img src={a23im2} alt="" />
+          <p>
+            TAE, located in leafy Foothill Ranch, south-east of Los Angeles, has
+            raised over $880m in private funding - more than any other fusion
+            company. High-profile backing has come from Goldman Sachs, the
+            Rockefeller family and the late Paul Allen, co-founder of Microsoft.
+            Its board of directors includes a former US Energy Secretary, Ernest
+            Moniz.
+          </p>
+          <p>
+            The company's 30m (100ft) -long fusion cylinder - called C2W
+            "Norman" after TAE's founder, physicist Norman Rostoker, who died in
+            2014 - represents a different approach to the doughnut-shaped
+            "tokamak" to be used for the world's biggest fusion experiment, the
+            multi-billion-euro ITER project.
+          </p>
+
+          <p>
+            Controlling plasma at tens of millions of degrees requires a
+            finely-tuned system. Google's expertise in machine learning - where
+            computer algorithms improve with experience - has been used for
+            "optimising" TAE's fusion device.
+          </p>
+
+          <p>
+            Optimisation, or tuning for best performance, is carried out when
+            something on the device changes, such as new hardware being added.
+            This process once took around two months, but with machine learning,
+            "we can now optimise in fractions of an afternoon," Dr Binderbauer
+            explained.
+          </p>
+          <img src={a23im3} alt="" />
+          <p>
+            "The way the rate of learning has accelerated is incredible and
+            allows us to make changes much more readily." Machine learning is
+            also used to reconstruct what's going on during a fusion experiment,
+            or "shot". Multiple strands of data can be pulled together for a
+            deeper understanding of the process.
+          </p>
+
+          <p>
+            "That's incredibly computer-dense and is a problem heretofore fairly
+            few people even attempted to attack," the CEO explains. He says the
+            results of the partnership with Google could shave a year from the
+            company's longer-term schedule, which envisages a commercial fusion
+            test device by 2030.
+          </p>
+
+          <p>
+            The company has already come a long way: Rostoker, a professor at
+            University of California Irvine, founded it as Tri-Alpha Energy in
+            1998. Austrian-born Binderbauer was one of Rostoker's PhD students,
+            and became the company's CEO four years ago. The two physicists
+            chose TAE's approach by starting with the requirements for a fusion
+            power plant and working backwards.
+          </p>
+
+          <p>
+            By using a hot, electrically-charged gas called plasma, fast-moving
+            particles can fuse, releasing energy. The $150m "Norman" device
+            crashes together two balls of plasma at supersonic speeds inside the
+            tube. Magnetic fields, in what's known as a Field-Reversed
+            Configuration (FRC), are used to control the process - which happens
+            in just 40 millionths of a second.
+          </p>
+
+          <p>
+            According to Prof Jeremy Chittenden, of Imperial College London, TAE
+            is "doing something quite different to what everyone else is doing".
+            Rather than relying on the heat of the plasma to generate
+            fast-moving particles for fusion, the device uses external particle
+            beams which are fired into the hot gas, similar to what happens in a
+            particle accelerator. "That's your fusion source," he explains.
+          </p>
+          <img src={a23im4} alt="" />
+          <p>
+            Fusion efforts such as ITER will use fuel consisting of deuterium
+            and tritium - two heavy versions of the element hydrogen. This
+            produces energy from fusion at tens of millions of degrees C, which
+            is still at a lower temperature than some other options. However,
+            there are downsides: tritium is radioactive, wears down the insides
+            of fusion reactors, and has a finite supply.
+          </p>
+
+          <p>
+            The "Norman" device powers its reactions with "regular" hydrogen and
+            deuterium - a more benign, if less potent choice. But these are good
+            surrogates for the fuel TAE eventually wants to move on to -
+            hydrogen and boron. This produces no neutron particles and therefore
+            little radioactivity, making machines straightforward to service and
+            maintain. But this fuel also requires extremely high temperatures.
+          </p>
+
+          <p>
+            C2W "Norman" operates at around 70 million degrees C, but
+            hydrogen-boron fuel requires temperatures to rise by a factor of
+            20-30, to several billion degrees C. It's a major challenge: "Powers
+            of ten are a big deal in science," says Binderbauer, "Can we get to
+            hydrogen-boron? I'm very convinced we can."
+          </p>
+          <img src={a23im5} alt="" />
+          <p>
+            Professor Roddy Vann, a plasma physicist at the University of York,
+            UK, who works on fusion with tokamaks, said: "Whilst you've got to
+            get the temperature right, the temperature and the density and the
+            energy confinement time all have to be sufficiently high,
+            simultaneously."
+          </p>
+
+          <p>
+            While neutrons do produce some radioactivity in the tokamak
+            structure, he explains, it's also their energy that we capture in
+            "conventional" deuterium-tritium fusion. However, said Prof Vann:
+            "If we could do aneutronic fusion, and could do it at temperatures
+            that were achievable, that would be very interesting."
+          </p>
+
+          <p>
+            Prof Chittenden explained: "Because they're not doing this through
+            heat, but through particle acceleration, that disadvantage of
+            [hydrogen-boron fuel] drops away," adding: "The net gain through
+            realising something as a commercial fusion plant is potentially
+            huge, because a vast fraction of the cost of a deuterium-tritium
+            plant is handling the radioactive products."
+          </p>
+
+          <p>
+            Dr Binderbauer says TAE's approach is also less susceptible to
+            turbulence - which hampers the ability to control plasmas in
+            tokamaks - and to energy leaking from the machine. In fact, the leak
+            rate goes down as the temperature in the device goes up, he says.
+            "As you get more energetic, the behaviour gets more manageable, more
+            predictable, more reliable," he says.
+          </p>
+
+          <p>
+            Two reactors are being planned to follow "Norman" - Copernicus and
+            Da Vinci. A key goal within a decade is to produce net energy, where
+            the output from fusion exceeds the energy supplied to initiate
+            reactions.
+          </p>
+
+          <p>
+            After nearly 25 years in existence, TAE has pioneered technology
+            with applications beyond just fusion. It is refining a technique
+            called boron neutron capture therapy with a view to its use in
+            cancer treatment. And it has formed a new division to commercialise
+            the power management systems from its work on C2W "Norman" for use
+            in the electric car and energy storage sectors.
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default A23;
